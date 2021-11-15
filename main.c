d2f23cc3df95496dfb534cb9b3ff72260fc01155
Eomhyunjun <zzp1318@gmail.com>
2021-11-15 12:14:56 +0900

    Start Philo -main 문 작성

---
+++ main.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eomhyeonjun <eomhyeonjun@student.42.fr>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/11 16:46:01 by eomhyeonjun       #+#    #+#             */
+/*   Updated: 2021/10/22 07:14:49 by eomhyeonjun      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include <unistd.h>
+
+void parsing_arg(int argc, char **argv)
+{
+       
+}
+
+int main(int argc, char **argv)
+{
+    parsing_arg(argc, argv);
+    
+    return (0);
+}
